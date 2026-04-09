e9f58f0a898e44249cf56ec8899ab421efc776ee
noramibu <50046813+noramibu@users.noreply.github.com>
2026-04-09 20:57:38 +0300

    26.1.2

--- src/main/java/me/noramibu/creepertweaks/CreeperTweaks.java
+++ src/main/java/me/noramibu/creepertweaks/CreeperTweaks.java
@@ -12,7 +12,7 @@ import org.slf4j.LoggerFactory;
 public class CreeperTweaks implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("creepertweaks");
     public static final String VERSION = "1.0.2";
-    public static final String MINECRAFT = "26.1.1";
+    public static final String MINECRAFT = "26.1.2";
 
     @Override
     public void onInitialize() {
